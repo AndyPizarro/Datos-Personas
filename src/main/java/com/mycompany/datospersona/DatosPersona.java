9633b07a48217e43b98135f428f140c138610c00
Cristian Andres <Cristian Andres@DESKTOP-QQPTLLE>
2024-01-16 11:49:58 -0500

    trabajo

---
+++ src/main/java/com/mycompany/datospersona/DatosPersona.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datospersona;
+
+/**
+ *
+ * @author Cristian Andres
+ */
+public class DatosPersona {
+
+    public static void main(String[] args) {
+        Estudiante estudiante1 = new Estudiante("Juan", 16, 1, "Software", null, null);
+        Estudiante estudiante2 = new Estudiante("Martha", 19, 5, "Mecánica", "martha@espe.adu.ec", null);
+        Estudiante estudiante3 = new Estudiante("Carlos", 17, 3, "Electrónica", null, null);
+        Estudiante estudiante4 = new Estudiante("Patricia", 20, 8, "Petroquímica", "patty@espe.edu.ec", null);
+
+        System.out.println(estudiante1.toString());
+        System.out.println(estudiante2.toString());
+        System.out.println(estudiante3.toString());
+        System.out.println(estudiante4.toString());
+    }
+ }
+
